991e7c912cb2ec2fb36fc4dbb7d14a3deb845adc
Sydney Palmer <spalmer7@binghamton.edu>
2023-06-12 21:49:11 -0400

    up

--- BudgetApp.py
+++ BudgetApp.py
@@ -1,6 +1,15 @@
 import pygame
+pygame.init()
 width = 500
 height = 500
-screen = pygame.display.set_mode((width, height))
-pygame.display.flip()
+display = pygame.display.set_mode((width, height))
+pygame.display.update()
 
+open = True
+while open:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT():
+            open = False
+
+pygame.quit()
+quit() 
