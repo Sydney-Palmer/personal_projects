69d0e56fd56beec3bb69331d263586bb26fb2530
Sydney Palmer <spalmer7@binghamton.edu>
2023-06-12 23:30:47 -0400

    name change

--- MindfulMoney.py
+++ MindfulMoney.py
@@ -3,12 +3,12 @@ pygame.init()
 width = 500
 height = 500
 display = pygame.display.set_mode((width, height))
-pygame.display.update()
+pygame.display.flip()
 
 open = True
 while open:
     for event in pygame.event.get():
-        if event.type == pygame.QUIT():
+        if event.type == pygame.QUIT:
             open = False
 
 pygame.quit()
